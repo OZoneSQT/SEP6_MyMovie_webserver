680a0ab12177dfcee313afc0443c8d8015658d2d
Michel Sommer <skruen@gmail.com>
2022-12-04 16:28:19 +0100

    SearchHandler created
    Note: Source Function is missing

---
+++ MyMovie/MyMovie/SearchHandler/Parser.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace MyMovie.SearchHandler
+{
+    // Source format:
+    // {"movie_results":[],"person_results":[],"tv_results":[],"tv_episode_results":[],"tv_season_results":[]}
+    public class Parser
+    {
+        /**
+         * Generated SearchModel from serchString
+         * @ Returns a SearcModel object contaning parsed search result
+         */
+        public SearchModel Parse(string jsonString) 
+        { 
+            SearchModel searchModel = new SearchModel();
+
+            dynamic result = JsonConvert.DeserializeObject(jsonString);
+
+            foreach (dynamic movie_results in result) {searchModel.AddMovie(movie_results);}
+            foreach (dynamic person_results in result) {searchModel.AddPerson(person_results);}
+            foreach (dynamic tv_results in result) {searchModel.AddTv(tv_results);}
+            foreach (dynamic tv_episode_results in result) {searchModel.AddTvEpisode(tv_episode_results);}
+            foreach (dynamic tv_season_results in result) {searchModel.AddTvSeason(tv_season_results);}
+
+            return searchModel;
+        }
+
+    }
+}
